6555a82485b4e24bd5dfa6beb8529d9c27d9b47b
Monkifoto LLc <monkifoto@gmail.com>
2023-10-25 22:22:21 -0700

    about me updates and portraits

--- abphoto/src/app/Pages/home/home.component.ts
+++ abphoto/src/app/Pages/home/home.component.ts
@@ -7,10 +7,10 @@ import { CarouselComponent } from '../carousel/carousel.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  url_0 = ["0", "../../../assets/home/IMG_8203.jpg", "Studio", "Diverse Studio"];
-  url_1 = ["1", "../../../assets/home/MKY05379-Edit.jpg", "Portraits", "Family and Individual Portraits"];
-  url_2 = ["2", "../../../assets/home/A7405937-Edit.jpg", "Headshots", "Amazing Headshots"];
-  url_3 = ["3", "../../../assets/home/MKI06844-Edit.jpg", "Product", "Product Photogaphy"];
+  url_0 = ["0", "../../../assets/home/IMG_8203.jpg", "Studio", "Consistency 100% of the time"];
+  url_1 = ["1", "../../../assets/home/MKY05379-Edit.jpg", "Portraits", "Showcase personality, character, or lifestyle"];
+  url_2 = ["2", "../../../assets/home/A7405937-Edit.jpg", "Headshots", "Convey professionalism and approachability"];
+  url_3 = ["3", "../../../assets/home/MKI06844-Edit.jpg", "Product", "Capturing the essence and details of a product"];
 
 constructor(){}
 
